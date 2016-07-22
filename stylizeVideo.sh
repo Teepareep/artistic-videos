2b89ce415f1e83748a512d7c6403acfa55cb068e
Nick Teeple <nicholas.teeple@gmail.com>
2016-07-22 11:51:37 -0700

    added logging , centralized directory storage
    moved flow, frames, output to different directories for centralizing content and making the process repeatable. 
    added logging for comparing process times and arguments

--- stylizeVideo.sh
+++ stylizeVideo.sh
@@ -19,35 +19,39 @@ if [ "$#" -le 1 ]; then
 fi
 
 # Parse arguments
-filename=$(basename "$1")
-extension="${filename##*.}"
+DATE=$(date -d "today" +"%Y%m%d%H%M")
+style_video=${1##*/}
+style_video_base=${style_video%.*}
+style_image=${2##*/}
+style_image_base=${style_image%.*}
+filename=${style_video_base}_${2##*/}
+extension="${style_video##*.}"
 filename="${filename%.*}"
 filename=${filename//[%]/x}
-style_image=$2
 
-# Create output folder
-mkdir -p $filename
+mkdir -p /content/output/$filename-$DATE
+mkdir -p /content/vid/$style_video_base/frames/
+mkdir -p /content/vid/$style_video_base/flow/
 
+touch /content/output/$filename-$DATE/$filename-$DATE.log.txt
+logfile=/content/output/$filename-$DATE/$filename-$DATE.log.txt
 
+echo "date is $(date "+%Y-%m-%d %H:%M:%S") " | tee -a $logfile
+echo "filename is" $filename | tee -a $logfile
+echo "style_video is " $style_video  | tee -a $logfile
+echo "style_video_base is " $style_video_base | tee -a $logfile
+echo "style_image  is" $style_image | tee -a $logfile
+echo "style_image_base  is" $style_image_base | tee -a $logfile
+echo "extension is" $extension | tee -a $logfile
+echo "output dir is" $filename-$DATE | tee -a $logfile
 echo ""
-read -p "Which backend do you want to use? \
-For Nvidia GPU, use cudnn if available, otherwise nn. \
-For non-Nvidia GPU, use clnn. Note: You have to have the given backend installed in order to use it. [nn] $cr > " backend
-backend=${backend:-nn}
+
+#For non-Nvidia GPU, use clnn. Note: You have to have the given backend installed in order to use it. [nn] $cr > " backend
+backend=cudnn
 
 if [ "$backend" == "cudnn" ]; then
   echo ""
   read -p "This algorithm needs a lot of memory. \
-  For a resolution of 450x350 you'll need roughly 2GB VRAM. \
-  VRAM usage increases linear with resolution. \
-  Please enter a resolution at which the video should be processed, \
-  in the format w:h, or leave blank to use the original resolution $cr > " resolution
-elif [ "$backend" = "nn" ] || [ "$backend" = "clnn" ]; then
-  echo ""
-  read -p "This algorithm needs a lot of memory. \
-  For a resolution of 450x350 you'll need roughly 4GB VRAM. \
-  VRAM usage increases linear with resolution. \
-  Maximum recommended resolution with a Titan X 12GB: 960:540. \
   Please enter a resolution at which the video should be processed, \
   in the format w:h, or leave blank to use the original resolution $cr > " resolution
 else
@@ -57,10 +61,10 @@ fi
 
 # Save frames of the video as individual image files
 if [ -z $resolution ]; then
-  $FFMPEG -i $1 ${filename}/frame_%04d.ppm
-  resolution=default
+  $FFMPEG -i $1 /content/vid/${style_video_base}/frames/frame_%04d.ppm
+  resolution=default  #how can we get actual numbers for this even if they are 'defaults'?
 else
-  $FFMPEG -i $1 -vf scale=$resolution ${filename}/frame_%04d.ppm
+  $FFMPEG -i $1 -vf scale=$resolution /content/vid/${style_video_base}/frames/frame_%04d.ppm
 fi
 
 echo ""
@@ -76,23 +80,42 @@ read -p "Enter the zero-indexed ID of the GPU to use, or -1 for CPU mode (very s
  [0] $cr > " gpu
 gpu=${gpu:-0}
 
+echo "------------------------------"
+echo "resolution : " ${resolution}  | tee -a $logfile
+echo "style_weight : " ${style_weight}  | tee -a $logfile
+echo " temporal_weight : " ${temporal_weight}  | tee -a $logfile
+
+
 echo ""
 echo "Computing optical flow. This may take a while..."
-bash makeOptFlow.sh ./${filename}/frame_%04d.ppm ./${filename}/flow_$resolution
+bash makeOptFlow.sh /content/vid/${style_video_base}/frames/frame_%04d.ppm /content/vid/${style_video_base}/flow/flow_$resolution
+
+
+echo "optical flow finished at $(date "+%Y-%m-%d %H:%M:%S") " | tee -a $logfile
+
+iterations_initial=1000
+iterations=400
+
+echo "iterations on initial frame: " $iterations_initial | tee -a $logfile
+echo "iterations on subsequent frames: " $iterations | tee -a $logfile
 
 # Perform style transfer
 th artistic_video.lua \
--content_pattern ${filename}/frame_%04d.ppm \
--flow_pattern ${filename}/flow_${resolution}/backward_[%d]_{%d}.flo \
--flowWeight_pattern ${filename}/flow_${resolution}/reliable_[%d]_{%d}.pgm \
+-content_pattern /content/vid/${style_video_base}/frames/frame_%04d.ppm \
+-flow_pattern /content/vid/${style_video_base}/flow/flow_${resolution}/backward_[%d]_{%d}.flo \
+-flowWeight_pattern /content/vid/${style_video_base}/flow/flow_${resolution}/reliable_[%d]_{%d}.pgm \
 -style_weight $style_weight \
 -temporal_weight $temporal_weight \
--output_folder ${filename}/ \
--style_image $style_image \
+-num_iterations $iterations_initial,$iterations \
+-output_folder /content/output/${filename}-$DATE/ \
+-style_image /content/img/$style_image \
 -backend $backend \
 -gpu $gpu \
 -cudnn_autotune \
 -number_format %04d
 
+echo "style transfer finished at $(date "+%Y-%m-%d %H:%M:%S") " | tee -a $logfile
+
 # Create video from output images.
-$FFMPEG -i ${filename}/out-%04d.png ${filename}-stylized.$extension
+$FFMPEG -i /content/output/${filename}-$DATE/out-%04d.png /content/output/${filename}-$DATE/${filename}-$DATE-stylized.$extension
+
